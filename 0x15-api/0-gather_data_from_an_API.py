078e385cdf4d195384782862940454d642e1aa4c
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-05-24 14:12:56 +0100

    gets a todo list and its titlea and total number plus the ones done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,20 +10,20 @@ import sys
 url = "https://jsonplaceholder.typicode.com/"
 emp_id = sys.argv[1]
 emp_url = f"{url}/users/{emp_id}"
-
 res = requests.get(emp_url)
 emp_data = res.json()
 emp_name = emp_data.get('name')
 
 todo_url = f"{url}/todos?userId={emp_id}"
 response = requests.get(todo_url)
-
 todos = response.json()
 total_todos = len(todos)
-done_task = [todo for todo in todos if todo.get('completed')]
+
+done_task = []
 total_done = 0
 for todo in todos:
     if todo.get('completed'):
+        done_task.append(todo)
         total_done += 1
 
 print(f"Employee {emp_name} is done with tasks({total_done}/{total_todos}):")
