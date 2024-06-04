3b3ca62b7be4d26907c496376772c2d40d9ce81a
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-06-04 15:52:39 +0100

    queries the Reddit API and returns top 10 hot titles

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/pyhton3
+""" queries the Reddit API
+prints the titles of the first 10 hot posts listed
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    try:
+        """method to print titles of the first 10
+        hot posts after query"""
+        url = f"https://www.reddit.com/r/{}/hot.json"
+        req = requests.get(url, header={"user-Agents":"My ReddiBot"})
+        data = req.json()
+        titles = data["titles"]["hot post"]
+        return titles
+    except Exception as err:
+        return None
