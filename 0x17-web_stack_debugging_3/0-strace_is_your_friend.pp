7f7561c6da4092be8eb13b732e744bb9c04b3da0
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-06-06 01:21:54 +0100

    Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Script to fix 500 server error when a GET HTTP method is requested to Apache web server
+
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+<h1>Web stack debugging #3</h1>
+<h3>Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet.</h3>
