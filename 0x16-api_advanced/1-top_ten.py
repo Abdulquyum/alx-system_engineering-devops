f70759d329e6c1e92cc3256ea0b0a5709ce6b9f6
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-06-07 10:59:00 +0100

    queries the Reddit API and returns top 10 hot titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -18,7 +18,7 @@ def top_ten(subreddit):
             children = response.json().get('data').get('children')
             for i in range(10):
                 print(children[i].get('data').get('title'))
-            else:
-                print("None")
+        else:
+            print("None")
     except Exception:
         print("None")
