fc1b2db98d6169f86fab1b64ccbfdb3a8d72d4b5
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-06-07 10:52:28 +0100

    queries the Reddit API and returns top 10 hot titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-""" queries the Reddit API
+''' queries the Reddit API
 prints the titles of the first 10 hot posts listed
-"""
+'''
 
 import requests
 
 
 def top_ten(subreddit):
     try:
-        """method to print titles of the first 10
-        hot posts after query"""
+        ''' method to print titles of the first 10
+        hot posts after query '''
         url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
         headers = {'User-Agent': 'Mozilla/5.0'}
         try:
             response = requests.get(url, headers=headers,
-                    allow_redirects=False)
+                                    allow_redirects=False)
             if response.status_code == 200:
                 children = response.json().get('data').get('children')
                 for i in range(10):
