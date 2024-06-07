b50ef6bc1c6a49c697167f9dd60152b242686f83
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-06-07 05:40:48 +0100

    a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,10 +10,16 @@ def top_ten(subreddit):
     try:
         """method to print titles of the first 10
         hot posts after query"""
-        url = f"https://www.reddit.com/r/{}/hot.json"
-        req = requests.get(url, header={"user-Agents": "My ReddiBot"})
-        data = req.json()
-        titles = data["titles"]["hot post"]
-        return titles
-    except Exception as err:
-        return None
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        try:
+            response = requests.get(url, headers=headers,
+                    allow_redirects=False)
+            if response.status_code == 200:
+                children = response.json().get('data').get('children')
+                for i in range(10):
+                    print(children[i].get('data').get('title'))
+                else:
+                    print("None")
+        except Exception:
+            print("None")
